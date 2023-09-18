a747f74bab8bd368501ece602a98d4d176ea7aa6
Collin Hazlett <hazlett.collin@gmail.com>
2023-09-17 22:48:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,13 +70,10 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        all_messages = [{"role": "system", "content": RuPaul_chatbot_prompt}]
+        all_messages = [{"role": "system", "content": RuPaul_chatbot_prompt},]
         for response in openai.ChatCompletion.create(
             model=st.session_state["openai_model"],
-            messages=all_messages.extend([
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ]),
+            messages= all_messages.extend([{"role": m["role"], "content": m["content"]} for m in st.session_state.messages]),
             stream=True,
         ):
             full_response += response.choices[0].delta.get("content", "")
