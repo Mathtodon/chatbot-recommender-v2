4fadc8dd770ef8e33879bbbee49ae3cdb092f315
Collin Hazlett <hazlett.collin@gmail.com>
2023-09-17 22:32:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ if "openai_model" not in st.session_state:
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    st.session_state.messages.append({"system": "user", "content": RuPaul_chatbot_prompt})
+    st.session_state.messages.append({"role": "system", "content": RuPaul_chatbot_prompt})
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
