a5be30da03de2f4845bfb47730061e628ea3a92c
Collin Hazlett <hazlett.collin@gmail.com>
2023-11-20 12:40:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ if prompt := st.chat_input("What can I help you with?"):
         ):
             #full_response += response.choices[0].delta.get("content", "")
             #full_response += response.choices[0].message.content
-            full_response += dict(response.choices[0].message).get("content", "")
+            full_response += completion.model_dump_json(indent=2).choices[0].message.get("content", "")
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
